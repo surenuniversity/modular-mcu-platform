07cb2770804b07da89fd6af3016c408d8f062a84
surenuniversity <118428177+surenuniversity@users.noreply.github.com>
2026-04-24 21:24:45 +0530

    Add fonts.h header file with font definitions

---
+++ src /fonts.h
@@ -0,0 +1,16 @@
+#ifndef __FONT_H
+#define __FONT_H
+
+#include <stdint.h>
+
+extern const uint8_t Font5x7[][5];
+
+#endif
+#ifndef FONTS_H
+#define FONTS_H
+
+#include <stdint.h>
+
+extern const uint8_t font5x7[][5];
+
+#endif
